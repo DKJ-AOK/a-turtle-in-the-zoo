35bcdcd4e89df6853c8fedee3c2c71fbdc77c5a2
Oetho <oe.thomsen@gmail.com>
2026-01-30 22:57:27 +0100

    Added Changes so Collision is now a thing

--- Header Files/Camera.h
+++ Header Files/Camera.h
@@ -22,7 +22,7 @@ public:
     // Positioning
     glm::vec3 Position;
     glm::vec3 Forward = glm::vec3(0.0f, 0.0f, -1.0f);
-    glm::vec3 Up      = glm::vec3(0.0f, 1.f, 0.0f);
+    glm::vec3 Up      = glm::vec3(0.0f, 1.0f, 0.0f);
     glm::vec3 Right   = glm::vec3(1.0f, 0.0f, 0.0f);
     glm::vec3 WorldUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
