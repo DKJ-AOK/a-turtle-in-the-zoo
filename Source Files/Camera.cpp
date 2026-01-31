b5c6c4c7d1821a71ab20519e47f42ab4f53ecf61
Oetho <oe.thomsen@gmail.com>
2026-01-31 21:02:28 +0100

    Moved all movement logic (flying and ground) to the PlayerController instead of in the camera. Making the Camera more reuseable to other things

--- Source Files/Camera.cpp
+++ Source Files/Camera.cpp
@@ -46,82 +46,12 @@ void Camera::Matrix(Shader &shader, const char *uniform) {
 }
 
 void Camera::HandleRotation(float moveDeltaX, float moveDeltaY) {
-    Yaw   += moveDeltaX * Sensitivity;
-    Pitch += moveDeltaY * Sensitivity;
+    Yaw   += moveDeltaX;
+    Pitch += moveDeltaY;
 
     // Clamp Pitch to avoid Gimbal Lock
     if (Pitch > 89.0f)  Pitch = 89.0f;
     if (Pitch < -89.0f) Pitch = -89.0f;
 
     UpdateCameraVectors();
-}
-
-void Camera::HandleGroundMovement(Action action, float deltaTime, World& world, glm::vec3 playerHalfExtent) {
-    float velocity = Speed * deltaTime;
-
-    // For landscape-movement (NOT IN USE YET)
-    glm::vec3 flatForward = glm::normalize(glm::vec3(Forward.x, 0.0f, Forward.z));
-    glm::vec3 flatRight = glm::normalize(glm::vec3(Right.x, 0.0f, Right.z));
-    auto previousPosition = Position;
-
-    if (action == Action::SPRINT)
-        Speed = SprintSpeed;
-    if (action == Action::WALK)
-        Speed = WalkSpeed;
-
-    HandleMovementXAxis(action, velocity, flatForward.x, flatRight.x, world, playerHalfExtent);
-    HandleMovementZAxis(action, velocity, flatForward.z, flatRight.z, world, playerHalfExtent);
-}
-
-void Camera::HandleMovementXAxis(Action action, float velocity, float flatForwardX, float flatRightX, World &world, glm::vec3 playerHalfExtent) {
-    auto previousPositionX = Position.x;
-
-    if (action == Action::MOVE_FORWARD)
-        Position.x += flatForwardX * velocity;
-    if (action == Action::MOVE_BACKWARD)
-        Position.x -= flatForwardX * velocity;
-    if (action == Action::MOVE_LEFT)
-        Position.x -= flatRightX * velocity;
-    if (action == Action::MOVE_RIGHT)
-        Position.x += flatRightX * velocity;
-
-    AABB playerBox = AABB::fromCenter(Position, playerHalfExtent);
-    AABB cubeBox;
-    if (world.checkCollision(playerBox, cubeBox))
-        Position.x = previousPositionX;
-}
-
-void Camera::HandleMovementZAxis(Action action, float velocity, float flatForwardZ, float flatRightZ, World &world, glm::vec3 playerHalfExtent) {
-    auto previousPositionZ = Position.z;
-
-    if (action == Action::MOVE_FORWARD)
-        Position.z += flatForwardZ * velocity;
-    if (action == Action::MOVE_BACKWARD)
-        Position.z -= flatForwardZ * velocity;
-    if (action == Action::MOVE_LEFT)
-        Position.z -= flatRightZ * velocity;
-    if (action == Action::MOVE_RIGHT)
-        Position.z += flatRightZ * velocity;
-
-    AABB playerBox = AABB::fromCenter(Position, playerHalfExtent);
-    AABB cubeBox;
-    if (world.checkCollision(playerBox, cubeBox))
-        Position.z = previousPositionZ;
-}
-
-void Camera::HandleFlyingMovement(Action action, float deltaTime) {
-    float velocity = Speed * deltaTime;
-
-    if (action == Action::MOVE_FORWARD)
-        Position += Forward * velocity;
-    if (action == Action::MOVE_BACKWARD)
-        Position -= Forward * velocity;
-    if (action == Action::MOVE_LEFT)
-        Position -= Right * velocity;
-    if (action == Action::MOVE_RIGHT)
-        Position += Right * velocity;
-    if (action == Action::SPRINT)
-        Speed = SprintSpeed;
-    if (action == Action::WALK)
-        Speed = WalkSpeed;
 }
