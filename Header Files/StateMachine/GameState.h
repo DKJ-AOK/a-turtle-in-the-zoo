b93b031c89d0def03564dc95ea431aab0a279176
Oetho <oe.thomsen@gmail.com>
2026-01-31 23:06:38 +0100

    Added GameState Abstract Class

---
+++ Header Files/StateMachine/GameState.h
@@ -0,0 +1,18 @@
+#ifndef A_TURTLE_IN_THE_ZOO_GAMESTATE_H
+#define A_TURTLE_IN_THE_ZOO_GAMESTATE_H
+#include "GameContext.h"
+
+class GameState {
+protected:
+    GameContext& gameContext;
+
+public:
+    explicit GameState(GameContext& ctx) : gameContext(ctx) {}
+    virtual ~GameState() = default;
+
+    virtual void onEnter() = 0;
+    virtual void update(float deltaTime) = 0;
+    virtual void onExit() = 0;
+};
+
+#endif //A_TURTLE_IN_THE_ZOO_GAMESTATE_H
